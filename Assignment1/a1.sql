8d821644846b0ce4e768bb4d770bf25033ad141f
weiqiang <409655689@qq.com>
2021-03-18 01:36:06 +0800

    update q11

--- Assignment1/a1.sql
+++ Assignment1/a1.sql
@@ -149,104 +149,85 @@ $body$
 
 -- 11
 create or replace procedure renew(pno integer) as
-$body$
+$$
 declare
-    p_ptype     char;
-    p_status    varchar(2);
-    p_effective date;
-    p_expire    date;
-    p_value     real;
-    p_comments  varchar(80);
-    p_sid       integer;
-    p_id        integer;
-    count       bigint;
-    countP      bigint;
-    newP        bigint;
-    countV      bigint;
-    flag        integer := 1;
+    cname_1 varchar(20);
 begin
+    if (select count(1) from policy p where p.pno = $1) = 0 then
 
-    select count(1) from policy p where p.pno = $1 into count;
-    select ptype,
-           status,
-           effectivedate,
-           expirydate,
-           agreedvalue,
-           comments,
-           sid,
-           id
-    from policy
-    into p_ptype, p_status, p_effective, p_expire, p_value, p_comments, p_sid, p_id;
-    select count(1)
-    from policy p
-    where p.pno = $1
-      and status not in ('D', 'RR', 'AR')
-      and expirydate > now()
-    into countP;
-
-    select count(id)
-    from policy
-    where ptype = ptype
-      and status not in ('D', 'RR', 'AR')
-      and expirydate > now()
-      and id = p_id
-    into countV;
-
-    if (count == 0) then
-        flag = 0;
-
-    elsif (countV > 1) then
-        flag = 0;
-    elsif (count > 0) then
-        update policy p set expirydate = now() where p.pno = $1;
-        insert into policy(ptype, status, effectivedate, expirydate, agreedvalue, comments, sid, id)
-        VALUES (p_ptype, p_status, now(), now() + (p_expire - p_effective), p_value, p_comments, p_sid, p_id);
-        select max(p.pno) from policy p into newP;
+    elsif (select count(1)
+           from policy p
+           where p.pno = $1
+             and status not in ('D', 'RR', 'AR')
+             and expirydate < now()) > 0 then
+        with temp as (select ptype,
+                             status,
+                             effectivedate,
+                             expirydate,
+                             agreedvalue,
+                             comments,
+                             sid,
+                             id
+                      from policy p
+                      where p.pno = $1)
+        insert
+        into policy(pno, ptype, status, effectivedate, expirydate, agreedvalue, comments, sid, id)
+        values ((select max(p.pno) from policy p) + 1, temp.ptype, temp.status, temp.effectivedate, temp.expirydate,
+                temp.agreedvalue, temp.comments, temp.sid,
+                temp.id);
         with new_coverage as (select * from coverage c where c.pno = $1)
         insert
-        into coverage(cname, maxamount, comments, pno)
-        values (new_coverage.cname, new_coverage.maxamount, new_coverage.comments, newP);
-    else
-        insert into policy(ptype, status, effectivedate, expirydate, agreedvalue, comments, sid, id)
-        values (p_ptype, p_status, now(), now() + (p_expire - p_effective), p_value, p_comments, p_sid, p_id);
-        select max(p.pno) from policy p into newP;
-        with new_coverage as (select * from coverage c where c.pno = $1)
+        into coverage(coid, cname, maxamount, comments, pno)
+        values ((select max(coid) from coverage) + 1, new_coverage.cname, new_coverage.maxamount, new_coverage.comments,
+                (select max(p.pno) from policy p));
+    elseif (select count(1)
+            from policy p
+            where p.pno = $1
+              and status not in ('D', 'RR', 'AR')
+              and expirydate > now()) > 0 then
+
+        with temp as (select ptype,
+                             status,
+                             effectivedate,
+                             expirydate,
+                             agreedvalue,
+                             comments,
+                             sid,
+                             id
+                      from policy p
+                      where p.pno = $1)
         insert
-        into coverage(cname, maxamount, comments, pno)
-        values (new_coverage.cname, new_coverage.maxamount, new_coverage.comments, newP);
+        into policy(pno, ptype, status, effectivedate, expirydate, agreedvalue, comments, sid, id)
+        values ((select max(p.pno) from policy p) + 1, (select ptype from temp), 'D',
+                now(),
+                now() + (select expirydate from temp) - (select effectivedate from temp),
+                (select agreedvalue from temp),
+                (select comments from temp),
+                (select sid from temp), (select id from temp));
+        update policy set expirydate = now() where pno = $1;
+        with new_coverage as (select * from coverage c where c.pno = $1 limit 1)
+        insert
+        into coverage(coid, cname, maxamount, comments, pno)
+        values ((select max(coid) from coverage) + 1, (select cname from new_coverage),
+                (select maxamount from new_coverage), (select comments from new_coverage),
+                (select max(p.pno) from policy p));
     end if;
-    if (flag = 0) then
-        rollback;
-    else
-        commit;
-    end if;
+    commit;
 end;
-$body$
-    LANGUAGE 'plpgsql';
+
+$$
+    language plpgsql;
+
 
 -- 12
-
 create function insure_create() returns trigger
 as
 $$
-declare
-    p_pid   integer;
-    p_pid_c integer;
 begin
-    select p2.pid
-    from policy
-             left join staff s on policy.sid = s.sid
-             left join person p2 on s.pid = p2.pid
-    where policy.pno = old.pno
-    into p_pid;
-    select p.pid
-    from client c
-             left join person p on c.pid = p.pid
-    where c.cid = old.cid
-    into p_pid_c;
-    if p_pid <> p_pid_c then
-        insert into insured_by(cid, pno) VALUES (new.cid, new.pno);
+    if (new.cid = (select sid from policy p where p.pno = new.pno)) then
+        raise exception 'same cid and sid';
     end if;
+    return new;
 end;
 $$
     language plpgsql;
@@ -257,64 +238,75 @@ create trigger check_insure
     for each row
 execute procedure insure_create();
 
-create function check_rate_create() returns trigger
-as
-$$
-declare
-    p_pid integer;
-begin
-    select
-    from person
-             left join staff s on person.pid = s.pid
-    where s.sid = old.sid
-    into p_pid;
-    if p_pid not in (select p.pid
-                     from rating_record rr
-                              left join coverage c on rr.coid = c.coid
-                              left join insured_by ib on c.pno = ib.pno
-                              left join client c2 on ib.cid = c2.cid
-                              left join person p on c2.pid = p.pid
-                     where rid = old.rid) then
-        insert into rated_by(sid, rid, rdate, comments)
-        values (old.sid, old.rid, old.rdate, old.comments);
-    end if;
-end;
-$$
-    language plpgsql;
-
-
-create trigger check_rate
-    before insert or update
-    on rated_by
-    for each row
-execute procedure check_rate_create();
-
-create function check_under_create() returns trigger as
-$$
-declare
-    p_pid integer;
-begin
-    select p.pid
-    from person p
-             left join staff s on s.pid = p.pid
-    into p_pid;
-    if p_pid not in (select p2.pid
-                     from underwriting_record ur
-                              left join policy p on ur.pno = p.pno
-                              left join insured_by ib on p.pno = ib.pno
-                              left join client c on ib.cid = c.cid
-                              left join person p2 on c.pid = p2.pid
-                     where ur.urid = old.urid) then
-        insert into underwritten_by(sid, urid, wdate, comments) values (old.sid, old.urid, old.wdate, old.comments);
-    end if;
-end;
-$$
-    language plpgsql;
-
-create trigger check_under
-    before insert or update
-    on underwritten_by
-    for each row
-execute procedure check_under_create();
+insert into insured_by (cid, pno)
+values (1, 7);
+-- 12
+--
+--
+-- create trigger check_insure
+--     before insert or update
+--     on insured_by
+--     for each row
+-- execute procedure insure_create();
+--
+-- create function check_rate_create() returns trigger
+-- as
+-- $$
+-- declare
+--     p_pid integer;
+-- begin
+--     select
+--     from person
+--              left join staff s on person.pid = s.pid
+--     where s.sid = old.sid
+--     into p_pid;
+--     if p_pid not in (select p.pid
+--                      from rating_record rr
+--                               left join coverage c on rr.coid = c.coid
+--                               left join insured_by ib on c.pno = ib.pno
+--                               left join client c2 on ib.cid = c2.cid
+--                               left join person p on c2.pid = p.pid
+--                      where rid = old.rid) then
+--         insert into rated_by(sid, rid, rdate, comments)
+--         values (old.sid, old.rid, old.rdate, old.comments);
+--     end if;
+-- end;
+-- $$
+--     language plpgsql;
+--
+--
+-- create trigger check_rate
+--     before insert or update
+--     on rated_by
+--     for each row
+-- execute procedure check_rate_create();
+--
+-- create function check_under_create() returns trigger as
+-- $$
+-- declare
+--     p_pid integer;
+-- begin
+--     select p.pid
+--     from person p
+--              left join staff s on s.pid = p.pid
+--     into p_pid;
+--     if p_pid not in (select p2.pid
+--                      from underwriting_record ur
+--                               left join policy p on ur.pno = p.pno
+--                               left join insured_by ib on p.pno = ib.pno
+--                               left join client c on ib.cid = c.cid
+--                               left join person p2 on c.pid = p2.pid
+--                      where ur.urid = old.urid) then
+--         insert into underwritten_by(sid, urid, wdate, comments) values (old.sid, old.urid, old.wdate, old.comments);
+--     end if;
+-- end;
+-- $$
+--     language plpgsql;
+--
+-- create trigger check_under
+--     before insert or update
+--     on underwritten_by
+--     for each row
+-- execute procedure check_under_create();
 
 
